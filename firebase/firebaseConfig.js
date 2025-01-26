ec3a07350d8954269c35ae0f6bc991a4fca73554
KalimsCodingWonderland <kalimqazi05@gmail.com>
2025-01-26 08:31:07 -0500

    .env stuff

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -3,15 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your Firebase configuration
+// Firebase configuration loaded from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC6y8XM4Zy0B7l4BxHeyfH4EUAGpRyCV1E",
-  authDomain: "test-3a9b1.firebaseapp.com",
-  projectId: "test-3a9b1",
-  storageBucket: "test-3a9b1.firebasestorage.app", // Fixed this value
-  messagingSenderId: "303585516916",
-  appId: "1:303585516916:web:48bd53a468ea84872ce021",
-  measurementId: "G-Z0GE5HXV5D"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -21,4 +21,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-export { auth, db, storage};
+export { auth, db, storage };
